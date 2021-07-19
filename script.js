52b0fe876f1285456ae851706d955a0d89c3c2f7
PeterSteinlesberger <petersteinlesberger@mail.de>
2021-07-19 03:22:23 +0200

    designed with CSS

---
+++ script.js
@@ -0,0 +1,22 @@
+
+let currentPokemon;
+let searchWithName;
+
+async function loadPokemon() {
+    let url = `https://pokeapi.co/api/v2/pokemon/${searchWithName}`;
+    let response = await fetch(url);
+    currentPokemon = await response.json();
+    renderPokemonInfo();
+}
+
+function renderPokemonInfo() {
+    document.getElementById('pokemonName').innerHTML = currentPokemon['name'];
+    
+
+}
+
+function searchByName() {
+    searchWithName = document.getElementById('inputField').value;
+    loadPokemon();
+    
+}
